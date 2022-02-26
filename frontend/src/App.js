f1bafdc80a2f98e739a2021fe44a125dba6974e2
ClaudiaZorrilla2343 <claudiaa2343@gmail.com>
2022-02-26 02:19:18 -0500

    Se creo el context y el reducer de la aplicación

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import { useReducer } from 'react';
+import { kataReducer } from './store/kataReducer';
+import { KataContext } from './store/kataContext';
 
 export const App = () => {
-  return (
-    <div>App</div>
-  )
+  const [state, dispatch] = useReducer(kataReducer, {});
 }
 
+return (
+  <KataContext.Provider value= {{state, dispatch}}>
+    <h2>Kata Crud</h2>
+  </KataContext.Provider>
+)
 
---
+++ frontend/src/store/kataReducer.js
@@ -0,0 +1,3 @@
+import { types } from '../types/types';
+
+export const kataReducer = (state = {}, action) => {};
