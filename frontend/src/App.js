70af8c44e75484f65c7177b609015b47778c1d97
ClaudiaZorrilla2343 <claudiaa2343@gmail.com>
2022-02-26 03:22:04 -0500

    Se agregaron los estilos iniciales de la página

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,14 +2,19 @@ import React from 'react'
 import { useReducer } from 'react';
 import { kataReducer } from './store/kataReducer';
 import { KataContext } from './store/kataContext';
+import { HomePage } from './components/HomePage';
+
+import './assets/sass/style.scss';
 
 export const App = () => {
   const [state, dispatch] = useReducer(kataReducer, {});
-}
 
-return (
-  <KataContext.Provider value= {{state, dispatch}}>
-    <h2>Kata Crud</h2>
-  </KataContext.Provider>
-)
+
+  return (
+    <KataContext.Provider value= {{state, dispatch}}>
+      <HomePage/>
+    </KataContext.Provider>
+  )
+
+}
 
---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,13 @@
+export const HomePage = () => {
+    return (
+      <div className='home-page'>
+        <div className='content'>
+          <h1>Title test text</h1>
+          <h2>Sub Title test text</h2>
+          <h3>Tasks test text</h3>
+          <p>Test test: Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit, dicta.</p>
+          <button>Button test text</button>
+        </div>
+      </div>
+    );
+  };
